a281825bfaea02e413a6be3b3ff8a0409ecabd9a
Nazima.Saherwala <nazima.saherwala@kahunasystems.com>
2019-06-26 12:50:02 +0530

    set user defaults

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,23 +10,33 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Buy cheese","Collect laundry","iron clothes"]
+   
+    var itemArray = ["Buy cheese","Collect laundry","iron clothes"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "ToDoList") as? [String] {
+            itemArray = items
+        }
     }
+    
     
     //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
+        
         return cell
     }
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -41,6 +51,27 @@ class ToDoListViewController: UITableViewController {
         
     }
 
-
+    
+    @IBAction func addItemPressed(_ sender: Any) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text ?? "")
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoList")
+            
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+    }
+    
 }
 
